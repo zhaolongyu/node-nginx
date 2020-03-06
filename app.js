ec09643f63fd0c7134c387d0aba13ba410a2fae2
zhaoyl <1486607544@qq.com>
2020-03-06 15:52:38 +0800

    图片

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ app.listen(3000,(res)=>{
 	console.log("成功")
 })
 
-app.get("/",(req,res)=>{
+app.get("/",(req,res)=>{  // 测试
 	res.send("1")
 })
-app.get("/ko",(req,res)=>{
+app.get("/ko",(req,res)=>{  //访问地址
 	res.send("ko----访问成功")
 })
-app.get("/bo",(req,res)=>{
+app.get("/bo",(req,res)=>{  // 失败
     res.send("0")
 })
